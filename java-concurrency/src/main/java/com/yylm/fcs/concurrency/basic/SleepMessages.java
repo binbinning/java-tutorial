e10d5b8befb4813269f237ef2429a4fa818bd1c8
宁江彬 <ningjiangbin@hfbank.com.cn>
2018-03-05 17:08:20 +0800

    simple thread usage

---
+++ java-concurrency/src/main/java/com/yylm/fcs/concurrency/basic/SleepMessages.java
@@ -0,0 +1,23 @@
+package com.yylm.fcs.concurrency.basic;
+
+/**
+ * Pausing Execution with Sleep
+ * 
+ * @See https://docs.oracle.com/javase/tutorial/essential/concurrency/sleep.html
+ *
+ */
+public class SleepMessages {
+
+	public static void main(String args[]) throws InterruptedException {
+		String importantInfo[] = { "Mares eat oats", "Does eat oats", "Little lambs eat ivy",
+				"A kid will eat ivy too" };
+
+		for (int i = 0; i < importantInfo.length; i++) {
+			// Pause for 4 seconds
+			Thread.sleep(4000);
+			// Print a message
+			System.out.println(importantInfo[i]);
+		}
+	}
+
+}
