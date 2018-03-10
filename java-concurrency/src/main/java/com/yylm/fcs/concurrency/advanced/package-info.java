7eae22aa48e825828dfa822dded02f1ad033f3d8
ningjb <binbinnjtu@163.com>
2018-03-10 20:21:10 +0800

    update package

---
+++ java-concurrency/src/main/java/com/yylm/fcs/concurrency/advanced/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * High Level Concurrency Object
+ * 
+ * @See https://docs.oracle.com/javase/tutorial/essential/concurrency/highlevel.html
+ *
+ */
+package com.yylm.fcs.concurrency.advanced;
