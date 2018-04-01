9ae44e4fcb2d5c6bcd1b92bf1dec37ed043546c9
ningjb <binbinnjtu@163.com>
2018-04-01 21:09:31 +0800

    differentiate override and overload

--- java-language/src/main/java/com/yylm/fcs/languanges/inheritance/Overriding_Hiding_Methods.java
+++ java-language/src/main/java/com/yylm/fcs/languanges/inheritance/Overriding_Hiding_Methods.java
@@ -33,6 +33,24 @@ public class Overriding_Hiding_Methods {
 		AnalyzeUtil.printMethodSignatures(myAnimal);
 		myAnimal.testClassMethod(); // 编译期解析 invokestatic
 		myAnimal.testInstanceMethod();
+
+		/**
+		 * 动态单分派，编译后方法签名已经确定，根据实际类型确定虚方法表的调用
+		 * 
+		 * <pre>
+		 *  27: invokevirtual #35 // Method com/yylm/fcs/languanges/inheritance/Animal_.sayHello:(Ljava/lang/Object;)V
+		 * </pre>
+		 */
+		myAnimal.sayHello("");
+
+		/**
+		 * 静态分派
+		 * 
+		 * <pre>
+		 33: invokevirtual #38 // Method com/yylm/fcs/languanges/inheritance/Cat.sayHello:(Ljava/lang/String;)V
+		 * </pre>
+		 */
+		myCat.sayHello("");
 	}
 }
 
@@ -43,6 +61,10 @@ class Animal_ {
 
 	public void testInstanceMethod() {
 		System.out.println("The instance method in Animal");
+	}
+
+	public void sayHello(Object obj) {
+		System.out.println("Hello Animal");
 	}
 }
 
@@ -55,6 +77,15 @@ class Cat extends Animal_ {
 	@Override
 	public void testInstanceMethod() {
 		System.out.println("The instance method in Cat");
+	}
+
+	@Override
+	public void sayHello(Object obj) {
+		System.out.println("Hello Cat with obj.");
+	}
+
+	public void sayHello(String obj) {
+		System.out.println("Hello Cat with string");
 	}
 
 }
